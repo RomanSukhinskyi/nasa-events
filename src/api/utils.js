c07efad18e82fd5dfbe48d15dc0c351e0dd009e7
romansyhinskiy <romansyhinskiy@gmail.com>
2021-10-21 10:31:44 +0300

    add cors url

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://eonet.sci.gsfc.nasa.gov/api/v3/events',
+    baseURL: 'https://cors-anywhere.herokuapp.com/https://eonet.sci.gsfc.nasa.gov/api/v3/events',
     responseType: 'json'
 }) 
-    // baseURL: 'https://cors-anywhere.herokuapp.com/https://eonet.sci.gsfc.nasa.gov/api/v3/events',
